f79d9d76a77cf1ed4653cd0a544fdd774f6f25c7
scoocyjuice <scoocyjuice@gmail.com>
2017-06-25 00:23:53 +0200

    * Added TicketAutomat * Edit Copyright on rp.java

---
+++ src/main/java/at/cyndergames/Interfaces/TicketAutomat.java
@@ -0,0 +1,12 @@
+package at.cyndergames.Interfaces;
+
+/**
+ * Created by Scoocyjuice on 25.06.2017.
+ * <p>
+ * #                          #
+ * #   Copyright by Minidodo  #
+ * #   and Scoocyjuice        #
+ * #                          #
+ */
+public interface TicketAutomat {
+}
