b654323c7433fe6346441604c39c972e4a76481f
Minidodo1 <meier2999@gmail.com>
2017-06-25 00:55:33 +0200

    * Added tickets beginning

---
+++ tickets/src/main/java/at/cyndergames/tickets/ticket.java
@@ -0,0 +1,12 @@
+package at.cyndergames.tickets;
+
+/**
+ * Created by Minidodo on 25.06.2017.
+ * <p>
+ * #                          #
+ * #   Copyright by Minidodo  #
+ * #   and Scoocyjuice        #
+ * #                          #
+ */
+public class ticket {
+}
