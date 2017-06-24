d1f3ad9dfe228a6253ec22804319a3514a0e2f31
Minidodo1 <meier2999@gmail.com>
2017-06-24 23:47:33 +0200

    Main-Commit

---
+++ src/main/java/at/cyndergames/rp.java
@@ -0,0 +1,22 @@
+package at.cyndergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Minidodo on 24.06.2017.
+ * <p>
+ * #                           #
+ * #   Copyright by Minidodo   #
+ * #                           #
+ */
+public class rp extends JavaPlugin{
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
