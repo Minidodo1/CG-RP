f4fee06ab7fe582c7a00d52b6985187ede4be5d7
Minidodo1 <meier2999@gmail.com>
2017-08-25 10:31:43 +0200

    * Not Important - Libs and new "Addon"

---
+++ GSM/src/main/java/at/cyndergames/GSM.java
@@ -0,0 +1,26 @@
+package at.cyndergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Minidodo on 23.08.2017.
+ * <p>
+ * #                          #
+ * #   Copyright by Minidodo  #
+ * #   and Scoocyjuice        #
+ * #                          #
+ */
+public class GSM extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
